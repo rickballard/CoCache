aa4d68f64341af5ecddf62562943445c40c7c817
Rick Ball <41898282+rickballard@users.noreply.github.com>
2025-10-18 11:06:54 -0400

    BPOE: guarded ANSI enable (scripts/Set-CoAnsi.ps1)
    BPOE-Dots: encouraged (Run-WithDotsEx)

---
+++ scripts/Set-CoAnsi.ps1
@@ -0,0 +1,5 @@
+$ErrorActionPreference='Stop'; Set-StrictMode -Version Latest
+try{
+  $supports = ($Host.Name -ne 'ConsoleHost') -or ($PSStyle.OutputRendering -ne 'PlainText')
+  if($supports){ $PSStyle.OutputRendering = 'ANSI' }
+}catch{}
